cdcc7b0869ea47904c388c450d0acad8eafa146c
Dev Pupil <devpupil@gmail.com>
2012-11-17 15:29:02 +0100

    second commit

--- Lottogenerator/src/de/devpupil/lottogenerator/MainActivity.java
+++ Lottogenerator/src/de/devpupil/lottogenerator/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         System.out.println("TESt");
+        System.out.println("nochmal");
     }
 
    
